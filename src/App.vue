2c61764f49bf5ebecf0a9b512e647081b28456a8
80004012 <80004012@sfmail.sf-express.com>
2019-11-07 20:07:10 +0800

    home page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <div id="app" class="container">
     <el-container>
-      <el-header></el-header>
+      <el-header v-if="isShowNav">nav</el-header>
       <el-main class="layout">
         <router-view />
       </el-main>
-      <el-footer></el-footer>
+      <el-footer v-if="isShowNav"></el-footer>
     </el-container>
   </div>
 </template>
+<script lang="ts">
+import { Vue, Component, Watch } from "vue-property-decorator";
+import { Route } from "vue-router";
 
+@Component
+export default class App extends Vue {
+  private isShowNav: boolean = true;
+  mounted(): void {
+    this.routeChange(this.$route, this.$route);
+  }
+  @Watch("$route")
+  routeChange(val: Route, oldVal: Route): void {
+    if (val.path == "/") {
+      this.isShowNav = false;
+    } else {
+      this.isShowNav = true;
+    }
+  }
+}
+</script>
 <style lang="less">
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,10 @@
     <div class="content">
       <div class="home_header">
         <router-link class="link" to="/home">
-          <img class="home_logo" src="../assets/userLogo.jpeg" alt="amei logo" />
+          <img class="home_logo" src="../assets/userLogo.jpg" alt="amei logo" />
         </router-link>
       </div>
+      <div class="home_header"></div>
     </div>
   </div>
 </template>
@@ -25,7 +26,19 @@ export default {
     .home_logo {
       width: 220px;
       border-radius: 50%;
+      animation: mylogo 5s infinite;
     }
+  }
+}
+@keyframes mylogo {
+  0% {
+    transform: scale(0.7, 0.7);
+  }
+  25% {
+    transform: scale(1, 1);
+  }
+  100% {
+    transform: scale(0.7, 0.7);
   }
 }
 </style>
